14d88a4cb2e7cd1cfd315a8a0cb365573c036825
linzhuofei <philip.f.lin@gmail.com>
2024-11-27 18:00:45 +0800

    新增自定義異常類

---
+++ src/main/java/com/philip/friendsbackend/exception/BusinessException.java
@@ -0,0 +1,34 @@
+package com.philip.friendsbackend.exception;
+
+import com.philip.friendsbackend.common.ErrorCode;
+
+/**
+ * 自定義異常類
+ *
+ * @author Philip
+ */
+public class BusinessException extends RuntimeException{
+    private final int code;
+
+    private final String description;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+        this.description = errorCode.getDescription();
+    }
+
+    public BusinessException(ErrorCode errorCode, String description) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
