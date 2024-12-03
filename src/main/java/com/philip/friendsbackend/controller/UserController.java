e69893ada103fd373495a037daa28591715906e7
linzhuofei <philip.f.lin@gmail.com>
2024-12-03 20:00:29 +0800

    優化返回的錯誤訊息

--- src/main/java/com/philip/friendsbackend/controller/UserController.java
+++ src/main/java/com/philip/friendsbackend/controller/UserController.java
@@ -51,7 +51,7 @@ public class UserController {
         String userAccount = userLoginRequest.getUserAccount();
         String userPassword = userLoginRequest.getUserPassword();
         if (StringUtils.isAnyBlank(userAccount, userPassword)){
-            return null;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         User user = userService.userLogin(userAccount, userPassword, request);
         return ResultUtils.success(user);
