6734e88875b94c1bf380a3dd9d791d0d86341ba3
linzhuofei <philip.f.lin@gmail.com>
2025-01-01 15:02:10 +0800

    隊伍和使用者資訊包裝類

---
+++ src/main/java/com/philip/friendsbackend/model/vo/TeamUserVO.java
@@ -0,0 +1,62 @@
+package com.philip.friendsbackend.model.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 隊伍和使用者資訊包裝類
+ *
+ * @author philip
+ */
+@Data
+public class TeamUserVO implements Serializable {
+
+    private static final long serialVersionUID = 8785394007192173075L;
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 隊伍名稱
+     */
+    private String name;
+
+    /**
+     * 簡介
+     */
+    private String description;
+
+    /**
+     * 最大人數
+     */
+    private Integer maxNum;
+
+    /**
+     * 使用者 id
+     */
+    private Long userId;
+
+    /**
+     * 0 - 公開，1 - 非公開，2 - 加密
+     */
+    private Integer status;
+
+    /**
+     * 創建時間
+     */
+    private Date createTime;
+
+    /**
+     * 更新時間
+     */
+    private Date updateTime;
+
+    /**
+     * 創建隊伍使用者資訊
+     */
+    private UserVO createUser;
+}
