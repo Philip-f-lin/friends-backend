3779aca72bfa6a51fece0ff12bea2478596f0181
linzhuofei <philip.f.lin@gmail.com>
2025-01-11 09:54:35 +0800

    編輯距離演算法測試(比較兩個 tagList

--- src/main/java/com/philip/friendsbackend/utils/AlgorithmUtils.java
+++ src/main/java/com/philip/friendsbackend/utils/AlgorithmUtils.java
@@ -1,5 +1,7 @@
 package com.philip.friendsbackend.utils;
 
+import java.util.List;
+
 /**
  * 演算法工具類
  *
@@ -8,6 +10,41 @@ package com.philip.friendsbackend.utils;
 public class AlgorithmUtils {
 
     /**
+     * 比較兩個 tagList
+     * 編輯距離演算法（用於計算兩個最相似的字串
+     * 參考：https://blog.csdn.net/tianjindong0804/article/details/115803158
+     * @param tagList1
+     * @param tagList2
+     * @return
+     */
+    public static int minDistance(List<String> tagList1, List<String> tagList2) {
+        if (tagList1 == null || tagList2 == null) {
+            throw new RuntimeException("參數不能為 null");
+        }
+        int[][] dp = new int[tagList1.size() + 1][tagList2.size() + 1];
+        // 初始化 DP 陣列
+        for (int i = 0; i <= tagList1.size(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= tagList2.size(); i++) {
+            dp[0][i] = i;
+        }
+        int cost;
+        for (int i = 1; i <= tagList1.size(); i++) {
+            for (int j = 1; j <= tagList2.size(); j++) {
+                if (tagList1.get(i - 1).equals(tagList2.get(j - 1))) {
+                    cost = 0;
+                } else {
+                    cost = 1;
+                }
+                dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + cost);
+            }
+        }
+        return dp[tagList1.size()][tagList2.size()];
+    }
+
+    /**
+     * 比較兩個字串
      * 編輯距離演算法（用於計算兩個最相似的字串
      * 參考：https://blog.csdn.net/tianjindong0804/article/details/115803158
      * @param word1
--- src/test/java/com/philip/friendsbackend/service/AlgorithmUtilsTest.java
+++ src/test/java/com/philip/friendsbackend/service/AlgorithmUtilsTest.java
@@ -4,11 +4,27 @@ import com.philip.friendsbackend.utils.AlgorithmUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 演算法工具類測試
  */
 @SpringBootTest
 public class AlgorithmUtilsTest {
+
+    @Test
+    void testCompareTags(){
+        List<String> tagList1 = Arrays.asList("Java", "三年", "男");
+        List<String> tagList2 = Arrays.asList("Python", "三年", "男");
+        List<String> tagList3 = Arrays.asList("Java", "二年", "女");
+
+        int score1 = AlgorithmUtils.minDistance(tagList1, tagList2);
+        int score2 = AlgorithmUtils.minDistance(tagList1, tagList3);
+
+        System.out.println(score1);// 1
+        System.out.println(score2);// 2
+    }
 
     @Test
     void test(){
